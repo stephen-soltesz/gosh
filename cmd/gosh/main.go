698e16c0193d270cead7c357fc46df9c6fc01eff
Stephen Soltesz <stephen.soltesz@gmail.com>
2018-08-08 22:14:37 -0400

    Add method to read and auth using private key

--- cmd/gosh/main.go
+++ cmd/gosh/main.go
@@ -5,41 +5,63 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"net"
-	"reflect"
-	"crypto/x509"
-	"encoding/pem"
+	"io/ioutil"
+	// "net"
+	// "reflect"
+	// "crypto/x509"
+	// "encoding/pem"
 
 
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/knownhosts"
 )
 
-func main() {
-	if len(os.Args) != 4 {
-		log.Fatalf("Usage: %s <user> <host:port> <command>", os.Args[0])
-	}
-
-	client, session, err := connectToHost(os.Args[1], os.Args[2])
-	if err != nil {
-		panic(err)
-	}
-	out, err := session.CombinedOutput(os.Args[3])
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(out))
-	client.Close()
+func init() {
+	log.SetFlags(log.LUTC|log.Lmicroseconds)
 }
 
-func connectToHost(user, host string) (*ssh.Client, *ssh.Session, error) {
-	var pass string
-	fmt.Print("Password: ")
-	fmt.Scanf("%s\n", &pass)
+func main() {
+	log.Println(os.Args)
+	if len(os.Args) != 5 {
+		log.Fatalf("Usage: %s <user> <host:port> <password> <command>", os.Args[0])
+	}
 
+	client, session, err := connectToHost(os.Args[1], os.Args[2], os.Args[3])
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Run:", os.Args[4])
+	out, err := session.CombinedOutput(os.Args[4])
+	if err != nil {
+		log.Println("output:", out)
+		panic(err)
+	}
+	log.Println("output:")
+	fmt.Println(string(out))
+	client.Close()
+	log.Println("closed")
+}
+
+func connectToHost(user, host, password string) (*ssh.Client, *ssh.Session, error) {
+	log.Println("hostkeys")
+	hostKeyCallback, err := knownhosts.New("mlab4.lga0t.txt")
+	if err != nil {
+		return nil, nil, err
+	}
+	signer, err := readPrivateKey("f1024")
+	if err != nil {
+		return nil, nil, err
+	}
 	sshConfig := &ssh.ClientConfig{
+		Config: ssh.Config{
+			RekeyThreshold: 1024 * 8,
+		},
 		User: user,
-		Auth: []ssh.AuthMethod{ssh.Password(pass)},
-		// HostKeyAlgorithms: []string{ssh.KeyAlgoRSA},
+		Auth: []ssh.AuthMethod{
+			// ssh.Password(password)
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: hostKeyCallback,
 	}
 	// ssh.FixedHostKey()
 	// accepts an ssh.PublicKey
@@ -57,25 +79,29 @@ func connectToHost(user, host string) (*ssh.Client, *ssh.Session, error) {
 	// reads a know_hosts file to implement the HostKeyCallback function.
 	// Same format as output from ssh-keyscan!
 
+	/*
 	hostKey, err := parseHostKey()
 	if err != nil {
 		panic(err)
 	}
+	*/
 	// Failing to match host key.
 	// sshConfig.HostKeyCallback = ssh.FixedHostKey(hostKey)
 	// sshConfig.HostKeyCallback = ssh.InsecureIgnoreHostKey()
-	sshConfig.HostKeyCallback = func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+	/*sshConfig.HostKeyCallback = func(hostname string, remote net.Addr, key ssh.PublicKey) error {
 		fmt.Println("known", hostKey)
 		fmt.Println("server", key)
 		fmt.Println("type", reflect.TypeOf(key))
 		return nil
-	}
+	}*/
 
+	log.Println("dial")
 	client, err := ssh.Dial("tcp", host, sshConfig)
 	if err != nil {
 		return nil, nil, err
 	}
 
+	log.Println("new session")
 	session, err := client.NewSession()
 	if err != nil {
 		client.Close()
@@ -85,26 +111,15 @@ func connectToHost(user, host string) (*ssh.Client, *ssh.Session, error) {
 	return client, session, nil
 }
 
-func parseHostKey() (ssh.PublicKey, error) {
-	const pubPEM = `
------BEGIN RSA PUBLIC KEY-----
-MIIBCgKCAQEAtcUQEdN/eMglvpi9CTyHWZa0ehGRBoqZaD4Nvckl3WOYW88B94Qm
-4MSMnon95oxUer1TV23Usrsq64XD8cxKazPibFA/hEcsWN4xPm9I7bBaar1BuKnS
-9a/vZRRqXuC1pjRmbb3y16SfMNW+8O7yUsVy9JZP5I9HDpM+/3xrfSgV1iGYSMzw
-lSFolimevEDbd6Mat6cHneyr7kmUrBeJkGJ1bWS04LzfMkay7/cYKLXH9jhEmweg
-DY+6albuC1/3sVsOOdN2taWBJ2guFK3TbEYkrUxF1cKqIciSTJplywLpMoeac69b
-EJ8PloqEII6mNAcWX2zAIPQMCcMA3Lt20QIDAQAB
------END RSA PUBLIC KEY-----`
-	
-	block, _ := pem.Decode([]byte(pubPEM))
-	if block == nil {
-		panic("failed to parse PEM block containing the public key")
-	}
-	
-	pub, err := x509.ParsePKCS1PublicKey(block.Bytes)
+func readPrivateKey(name string) (ssh.Signer, error) {
+	pem, err := ioutil.ReadFile(name)
 	if err != nil {
-		panic("failed to parse DER encoded public key: " + err.Error())
+		return nil, err
 	}
 
-	return ssh.NewPublicKey(pub)
+	signer, err := ssh.ParsePrivateKey(pem)
+	if err != nil {
+		return nil, err
+	}
+	return signer, nil
 }
