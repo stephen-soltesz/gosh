48f5cd0ac985d5b8e758f1747a4fdcb6c34a4f78
Stephen Soltesz <stephen.soltesz@gmail.com>
2018-10-07 22:14:31 -0400

    add notes about racadm limits

--- cmd/gosh/main.go
+++ cmd/gosh/main.go
@@ -94,32 +94,43 @@ func connectToHost(user, host, password string) (*ssh.Client, error) {
 	// one of *rsa.PublicKey
 	// TODO: how to deserialize an ssh public key?
 	//
+//		Config: ssh.Config{
+//			RekeyThreshold: 1024 * 8,
+//		},
+
 	// $ ssh-keyscan -p 806  mlab3d.lga03.measurement-lab.org > host.txt
 	// $ ssh-keygen -f mlab3d.lga03.pub -e -m pem > mlab3d.lga03.pem
-
 	// Add user public key to the DRAC. to authenticate with private key.
 	// https://www.dell.com/support/manuals/us/en/04/integrated-dell-remote-access-cntrllr-8-with-lifecycle-controller-v2.00.00.00/racadm_idrac_pub-v1/sshpkauth?guid=guid-be12abd1-4995-4fa3-b090-9cb41321b7a4&lang=en-us
 
 	// TODO:
+	// read know_hosts file to implement the HostKeyCallback function.
 	// ssh/knownhosts.New -- https://godoc.org/golang.org/x/crypto/ssh/knownhosts#New
-	// reads a know_hosts file to implement the HostKeyCallback function.
 	// Same format as output from ssh-keyscan!
 
-	/*
-		hostKey, err := parseHostKey()
-		if err != nil {
-			panic(err)
-		}
-	*/
 	// Failing to match host key.
 	// sshConfig.HostKeyCallback = ssh.FixedHostKey(hostKey)
 	// sshConfig.HostKeyCallback = ssh.InsecureIgnoreHostKey()
-	/*sshConfig.HostKeyCallback = func(hostname string, remote net.Addr, key ssh.PublicKey) error {
-		fmt.Println("known", hostKey)
-		fmt.Println("server", key)
-		fmt.Println("type", reflect.TypeOf(key))
-		return nil
-	}*/
+	// sshConfig.HostKeyCallback = func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+	//	fmt.Println("known", hostKey)
+	//	fmt.Println("server", key)
+	//	fmt.Println("type", reflect.TypeOf(key))
+	//	return nil
+	// }
+
+	// NOTE: sending a racadm command with more than 256 chars fails due to the
+	// interpreter getting a partial string. Methods tried:
+	// * session.CombinedOutput()
+	// * ssh admin@drac.fqdn "racadm sshpkauth ..."
+	// * echo "racadm sshpkauth ..." | ssh admin@drac.fqdn
+    // Only the last method works. It is unknown whether that mechanism can be
+    // simulated with the go ssh package.
+
+	/*
+	Work around the apparent 256-byte line limit using other methods:
+        echo "racadm sshpkauth -i 2 -k 1 -t 'ssh-rsa <key>'" | ssh -p806 \
+		    admin@mlab4d.lga0t.measurement-lab.org
+	*/
 
 	log.Println("dial")
 	client, err := ssh.Dial("tcp", host, sshConfig)
